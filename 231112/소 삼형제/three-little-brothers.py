5db795c4cb919e16f4ba6f602d9598f4a5251f6b
osy9757 <37167892+osy9757@users.noreply.github.com>
2023-11-13 08:48:06 +0900

    Title: 소 삼형제, Time: 135ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 231112/소 삼형제/three-little-brothers.py
@@ -0,0 +1,15 @@
+N = int(input())
+names = [list(map(str, input().split())) for _ in range(N)]
+
+for name in names:
+    name.sort()
+names.sort()
+count=1
+maxCount=0
+for i in range(N-1):
+    if names[i] == names[i+1]:
+        count+=1
+    else:
+        maxCount=max(count,maxCount)
+        count=1
+print(maxCount)
