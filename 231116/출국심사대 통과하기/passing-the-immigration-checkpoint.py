cb350f9117e70da036c23b46b6f36f6f414bdd14
osy9757 <37167892+osy9757@users.noreply.github.com>
2023-11-16 09:24:44 +0900

    Title: 출국심사대 통과하기, Time: 152ms, Memory: 24MB, Status: Passed - CodeTree

--- 231116/출국심사대 통과하기/passing-the-immigration-checkpoint.py
+++ 231116/출국심사대 통과하기/passing-the-immigration-checkpoint.py
@@ -1,8 +1,6 @@
 N = int(input())
-checkTime = [list(map(int,input().split())) for _ in range(N)]
-checkTime.sort()
-for i in range(0,N-1):
+checkTime = sorted([list(map(int, input().split())) for _ in range(N)])
+for i in range(N - 1):
     checkTime[i][1] += checkTime[i][0]
-    if checkTime[i+1][0] <= checkTime[i][1]:
-        checkTime[i+1][0] = checkTime[i][1]
-print(checkTime[-1][1]+ checkTime[-1][0])
+    checkTime[i + 1][0] = max(checkTime[i + 1][0], checkTime[i][1])
+print(sum(checkTime[-1]))
