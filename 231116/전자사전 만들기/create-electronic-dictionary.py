09c8bf332f6068ca3f757879e712fc1cd7c375b4
osy9757 <37167892+osy9757@users.noreply.github.com>
2023-11-16 14:44:39 +0900

    Title: 전자사전 만들기, Time: 120ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231116/전자사전 만들기/create-electronic-dictionary.py
@@ -0,0 +1,14 @@
+N, T = map(int,input().split())
+words = [input() for _ in range(N)]
+sorted_words = sorted(words)
+
+for _ in range(T):
+    pass_num, search_word = map(str, input().split())
+    for sorted_word in sorted_words:
+        if sorted_word[:len(search_word)] == search_word:
+            if sorted_words.index(sorted_word)+int(pass_num)-1>=len(sorted_words) or sorted_words[sorted_words.index(sorted_word)+int(pass_num)][:len(search_word)] != search_word:
+                print(-1)
+                break
+            else:
+                print(words.index(sorted_words[sorted_words.index(sorted_word)+int(pass_num)-1])+1) 
+                break
