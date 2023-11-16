855e2bfcbc783b3f72d185fa882b4443b1f95074
osy9757 <37167892+osy9757@users.noreply.github.com>
2023-11-16 15:14:18 +0900

    Title: 전자사전 만들기, Time: 5093ms, Memory: 27MB, Status: Time Limit Exceed - CodeTree

--- 231116/전자사전 만들기/create-electronic-dictionary.py
+++ 231116/전자사전 만들기/create-electronic-dictionary.py
@@ -1,16 +1,18 @@
-N, T = map(int,input().split())
+N, T = map(int, input().split())
 words = [input() for _ in range(N)]
-sorted_words = sorted(words)
+indexed_words = [(word, i) for i, word in enumerate(words)]
+sorted_indexed_words = sorted(indexed_words, key=lambda x: x[0])
 
 for _ in range(T):
     pass_num, search_word = map(str, input().split())
-    for sorted_word in sorted_words:
-        if sorted_word[:len(search_word)] == search_word:
-            if sorted_words.index(sorted_word)+int(pass_num)-1>=len(sorted_words) or sorted_words[sorted_words.index(sorted_word)+int(pass_num)-1][:len(search_word)] != search_word:
-                print(-1)
+    pass_num = int(pass_num)
+    found = False
+    for sorted_word, idx in sorted_indexed_words:
+        if sorted_word.startswith(search_word):
+            pass_num -= 1
+            if pass_num == 0:
+                print(idx + 1)
+                found = True
                 break
-            else:
-                print(words.index(sorted_words[sorted_words.index(sorted_word)+int(pass_num)-1])+1) 
-                break
-    else:
+    if not found:
         print(-1)
