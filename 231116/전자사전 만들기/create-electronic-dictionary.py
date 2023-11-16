44ebd28279e0f7215d37fa4c6b3df9283b3dfab2
osy9757 <37167892+osy9757@users.noreply.github.com>
2023-11-16 15:18:38 +0900

    Title: 전자사전 만들기, Time: 546ms, Memory: 28MB, Status: Passed - CodeTree

--- 231116/전자사전 만들기/create-electronic-dictionary.py
+++ 231116/전자사전 만들기/create-electronic-dictionary.py
@@ -1,32 +1,27 @@
-N, T = map(int, input().split())
-words = [input().strip() for _ in range(N)]
-indexed_words = sorted((word, i) for i, word in enumerate(words))
-
-def binary_search(prefix, find_last=False):
-    left, right = 0, len(indexed_words) - 1
-    result = -1
+def binary_search(array, prefix):
+    left, right = 0, len(array) - 1
     while left <= right:
         mid = (left + right) // 2
-        if indexed_words[mid][0].startswith(prefix):
-            result = mid
-            if find_last:
-                left = mid + 1
-            else:
-                right = mid - 1
-        elif indexed_words[mid][0] < prefix:
+        if array[mid].startswith(prefix):
+            if mid == 0 or not array[mid - 1].startswith(prefix):
+                return mid
+            right = mid - 1
+        elif array[mid] < prefix:
             left = mid + 1
         else:
             right = mid - 1
-    return result
+    return -1
+
+N, T = map(int, input().split())
+words = [input() for _ in range(N)]
+sorted_words = sorted(words)
 
 for _ in range(T):
-    k, prefix = input().split()
-    k = int(k)
-
-    first = binary_search(prefix)
-    last = binary_search(prefix, find_last=True)
-
-    if first == -1 or last - first + 1 < k:
+    pass_num, search_word = map(str, input().split())
+    pass_num = int(pass_num)
+    
+    start_index = binary_search(sorted_words, search_word)
+    if start_index == -1 or start_index + pass_num - 1 >= len(sorted_words) or not sorted_words[start_index + pass_num - 1].startswith(search_word):
         print(-1)
     else:
-        print(indexed_words[first + k - 1][1] + 1)
+        print(words.index(sorted_words[start_index + pass_num - 1]) + 1)
